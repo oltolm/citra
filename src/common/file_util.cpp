83ac767fbbe31a2cd33e13e57eb20c95ed7ca4eb
oltolm <oleg.tolmatcev@gmail.com>
2025-05-18 10:50:07 +0200

    file_util.cpp: fix stat on Windows

--- src/common/file_util.cpp
+++ src/common/file_util.cpp
@@ -112,7 +112,7 @@ bool Exists(const std::string& filename) {
     StripTailDirSlashes(copy);
 
 #ifdef _WIN32
-    struct stat file_info;
+    struct _stat64 file_info;
     // Windows needs a slash to identify a driver root
     if (copy.size() != 0 && copy.back() == ':')
         copy += DIR_SEP_CHR;
@@ -133,18 +133,20 @@ bool IsDirectory(const std::string& filename) {
     return AndroidStorage::IsDirectory(filename);
 #endif
 
-    struct stat file_info;
-
     std::string copy(filename);
     StripTailDirSlashes(copy);
 
 #ifdef _WIN32
+    struct _stat64 file_info;
+
     // Windows needs a slash to identify a driver root
     if (copy.size() != 0 && copy.back() == ':')
         copy += DIR_SEP_CHR;
 
     int result = _wstat64(Common::UTF8ToUTF16W(copy).c_str(), &file_info);
 #else
+    struct stat file_info;
+
     int result = stat(copy.c_str(), &file_info);
 #endif
 
@@ -385,10 +387,11 @@ u64 GetSize(const std::string& filename) {
         return 0;
     }
 
-    struct stat buf;
 #ifdef _WIN32
+    struct _stat64 buf;
     if (_wstat64(Common::UTF8ToUTF16W(filename).c_str(), &buf) == 0)
 #elif ANDROID
+    struct stat buf;
     u64 result = AndroidStorage::GetSize(filename);
     LOG_TRACE(Common_Filesystem, "{}: {}", filename, result);
     return result;
