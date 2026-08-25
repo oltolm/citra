2f86703b66fc68f093ef9876e88112f273108732
Oleg Tolmatcev <oleg.tolmatcev@gmail.com>
2026-08-25 18:59:16 +0200

    externals: remove catch2

--- externals/cmake-modules/CitraHandleSystemLibs.cmake
+++ externals/cmake-modules/CitraHandleSystemLibs.cmake
@@ -21,7 +21,6 @@ option(USE_SYSTEM_CUBEB "Use the system cubeb (instead of the bundled one)" OFF)
 option(USE_SYSTEM_LODEPNG "Use the system lodepng (instead of the bundled one)" OFF)
 option(USE_SYSTEM_VMA "Use the system VulkanMemoryAllocator (instead of the bundled one)" OFF)
 option(USE_SYSTEM_VULKAN_HEADERS "Use the system Vulkan headers (instead of the bundled ones)" OFF)
-option(USE_SYSTEM_CATCH2 "Use the system Catch2 (instead of the bundled one)" OFF)
 
 # Qt and MoltenVK are handled separately
 CMAKE_DEPENDENT_OPTION(DISABLE_SYSTEM_SDL2 "Disable system SDL2" OFF "USE_SYSTEM_LIBS" OFF)
