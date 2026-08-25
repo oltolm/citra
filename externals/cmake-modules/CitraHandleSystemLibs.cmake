20d1c1000f96c13bf106f04b04ac92a44ab1ae57
Oleg Tolmatcev <oleg.tolmatcev@gmail.com>
2026-08-25 19:57:37 +0200

    externals: remove vma

--- externals/cmake-modules/CitraHandleSystemLibs.cmake
+++ externals/cmake-modules/CitraHandleSystemLibs.cmake
@@ -16,7 +16,6 @@ option(USE_SYSTEM_ENET "Use the system libenet (instead of the bundled one)" OFF
 option(USE_SYSTEM_CRYPTOPP "Use the system cryptopp (instead of the bundled one)" OFF)
 option(USE_SYSTEM_CUBEB "Use the system cubeb (instead of the bundled one)" OFF)
 option(USE_SYSTEM_LODEPNG "Use the system lodepng (instead of the bundled one)" OFF)
-option(USE_SYSTEM_VMA "Use the system VulkanMemoryAllocator (instead of the bundled one)" OFF)
 
 # Qt and MoltenVK are handled separately
 CMAKE_DEPENDENT_OPTION(DISABLE_SYSTEM_SDL2 "Disable system SDL2" OFF "USE_SYSTEM_LIBS" OFF)
